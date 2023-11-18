68bf615885cbdf5f7aecf85f1fe783c93525293d
jainish021 <jainish1999@gmail.com>
2023-11-18 15:43:11 -0600

    Removed a redux slice

--- store/store.js
+++ store/store.js
@@ -2,14 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import friendProfileVisibilityReducer from '../slices/friendProfileVisibilitySlice'
 import selectedItemReducer from '../slices/selectedItemSlice'
 import userInformationReducer from '../slices/userInformationSlice'
-import socketReducer from '../slices/socketSlice'
 
 const store = configureStore({
     reducer: {
         friendProfileVisibility: friendProfileVisibilityReducer,
         selectedItem: selectedItemReducer,
-        userInformation: userInformationReducer,
-        socket: socketReducer
+        userInformation: userInformationReducer
     },
 })
 
